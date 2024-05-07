51fdb104be6936cb49b12e9768a2106f2354005a
Z82-10-20 <zakfran@hotmail.co.uk>
2024-05-07 23:21:07 +0800

    26th commit

---
+++ backend/config/ecosystem.config.cjs
@@ -0,0 +1,17 @@
+// ecosystem.config.js
+module.exports = {
+  apps : [{
+    name: "dadajon",
+    script: ".C:\\Users\\zakfr\\OneDrive\\Desktop\\dadajon\\backend\\server.js",
+    watch: true,
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+};
--- backend/routes/flightRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getFlightSchedules } from '../controllers/flightControllers';
-
-const router = express.Router();
-
-router.get('/flights', getFlightSchedules);
-
-export default router;
