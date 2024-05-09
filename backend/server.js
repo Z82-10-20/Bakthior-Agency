75534414cba42b0fe2d31d49be37ab77588f66da
Z82-10-20 <zakfran@hotmail.co.uk>
2024-05-09 22:39:03 +0800

    2nd commit

--- backend/server.js
+++ backend/server.js
@@ -19,32 +19,29 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-    app.get('/', (req, res) => {
+app.use('/api/contact', contactRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+  });
+}
+
+app.get('/', (req, res) => {
   res.send('Server is running');
 });
-
-
-app.use('/api/contact', contactRoutes);
 
 app.use((error, req, res, next) => {
   console.error('Unhandled error:', error);
   res.status(500).send('An unexpected error occurred');
 });
 
-if (process.env.NODE_ENV === 'production') {
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-  );
-}
-
-const port = process.env.PORT || 5001;
-// Start the server and handle the error
 const server = app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
@@ -54,8 +51,6 @@ server.on('error', (error) => {
     console.error(`Port ${port} is already in use.`);
     process.exit(1);
   } else {
-    app.get('/', (req, res) => {
-  res.send('Server is running');
-});
+    throw error;
   }
 });
